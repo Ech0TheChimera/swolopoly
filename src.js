413f7373d609aa480ae530384b8affaf0f8eedbb
Ech0TheChimera <AaronWhittington8284@gmail.com>
2017-05-05 10:13:29 -0700

    attempted to generate board

--- src.js
+++ src.js
@@ -1,4 +1,18 @@
-var board = new Array(11,4);
+var board = new Array(11);
+
+var gameBoard = "";
 for (i of board){
-	console.log(i);
+	if (i === 0 || i === 10){
+		board[i] = new Array(11);
+	}
 }
+
+for (i of board) {
+	for (j of i){
+		gameBoard = gameBoard + ("<rect width=5 height=5 x="+((i*10)+10)+" y="+((j*10)+10)+"></rect>");
+	}
+}
+
+var svg = $("svg");
+
+svg.html(gameBoard);
