efe4e367a247f71edd033fbf9a46524bc7627190
Ech0TheChimera <AaronWhittington8284@gmail.com>
2017-05-06 18:39:38 -0700

    Updated to test nesting objects in array w/ benchpress and player objects, tested w/ for loop

--- src.js
+++ src.js
@@ -1,3 +1,5 @@
+/*
+
 var board = new Array(11);
 
 var gameBoard = "";
@@ -7,12 +9,46 @@ for (i of board){
 	}
 }
 
+x = 0;
+for (i of board){
+	x++;
+	gameBoard += ("<rect width=5 height=5 x="+5*x+" y=5 />");
+}
+
+/*
 for (i of board) {
 	for (j of i){
 		gameBoard += ("<rect width=5 height=5 x="+((i*10)+10)+" y="+((j*10)+10)+" />");
 	}
 }
 
+*/
+
 var svg = $("svg");
 
-svg.html(gameBoard);
+//svg.html(gameBoard);
+
+
+var game = [
+	[
+		{
+			type:"benchpress",
+			rent:500,
+			cost:700,
+			owned:false
+		},
+		{
+			type:"player",
+			life:100,
+			gains:0
+		}
+	]
+];
+
+for (i of game){
+	for (j of i){
+		if (j.type === "benchpress"){
+			alert(j.rent);
+		}
+	}
+}
