89364b7e61d11859ba665456ebbd17be06b170d3
Ech0TheChimera <AaronWhittington8284@gmail.com>
2017-05-09 13:56:10 -0700

    Basic grid functionality added

--- src.js
+++ src.js
@@ -1,54 +1,26 @@
-/*
 
-var board = new Array(11);
+// init board x
+var game = new Array(11);
 
-var gameBoard = "";
-for (i of board){
-	if (i === 0 || i === 10){
-		board[i] = new Array(11);
-	}
+// init board y
+for (i=0;i<game.length;i++){
+	game[i] = new Array(11);
 }
 
-x = 0;
-for (i of board){
-	x++;
-	gameBoard += ("<rect width=5 height=5 x="+5*x+" y=5 />");
-}
 
-/*
-for (i of board) {
-	for (j of i){
-		gameBoard += ("<rect width=5 height=5 x="+((i*10)+10)+" y="+((j*10)+10)+" />");
-	}
-}
-
-*/
-
-var svg = $("svg");
-
-//svg.html(gameBoard);
+var gameBoardGen = ""; // holds content to eventually be pushed into SVG DOM
 
 
-var game = [
-	[
-		{
-			type:"benchpress",
-			rent:500,
-			cost:700,
-			owned:false
-		},
-		{
-			type:"player",
-			life:100,
-			gains:0
-		}
-	]
-];
-
-for (i of game){
-	for (j of i){
-		if (j.type === "benchpress"){
-			alert(j.rent);
+for (i=0;i<game.length;i++) {
+	for (j=0;j<game[i].length;j++){
+		if ((i === 0 || i === 10) || (j === 0 || j === 10)){
+			gameBoardGen += ("<rect width=50 height=50 x="+(i*50)+" y="+(j*50)+" gridx="+i+" gridy="+j+" fill='white' stroke='black' />");
 		}
 	}
 }
+
+
+
+var svg = $("svg"); //Define element we will write to
+
+svg.html(gameBoardGen); //render board from gameBoardGen
