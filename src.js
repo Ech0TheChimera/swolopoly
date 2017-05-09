22089c5a5bfb60ef5bdde4d552a9ca5509864d63
Ech0TheChimera <AaronWhittington8284@gmail.com>
2017-05-09 14:02:28 -0700

    Updated draw grid to be in a function

--- src.js
+++ src.js
@@ -1,26 +1,24 @@
+var svg = $("svg"); //Define element we will write to
 
 // init board x
 var game = new Array(11);
+
 
 // init board y
 for (i=0;i<game.length;i++){
 	game[i] = new Array(11);
 }
 
-
-var gameBoardGen = ""; // holds content to eventually be pushed into SVG DOM
-
-
-for (i=0;i<game.length;i++) {
-	for (j=0;j<game[i].length;j++){
-		if ((i === 0 || i === 10) || (j === 0 || j === 10)){
-			gameBoardGen += ("<rect width=50 height=50 x="+(i*50)+" y="+(j*50)+" gridx="+i+" gridy="+j+" fill='white' stroke='black' />");
+function drawBoard(){
+	var gameBoardGen = ""; // holds content to eventually be pushed into SVG DOM
+	for (i=0;i<game.length;i++) {
+		for (j=0;j<game[i].length;j++){
+			if ((i === 0 || i === 10) || (j === 0 || j === 10)){ // ONLY DRAW IF THE GRID IS ON THE FAR LEFT, RIGHT, TOP OR BOTTOM
+				gameBoardGen += ("<rect width=50 height=50 x="+(i*50)+" y="+(j*50)+" gridx="+i+" gridy="+j+" fill='white' stroke='black' />"); // draw 
+			}
 		}
 	}
+	svg.html(gameBoardGen); //render board from gameBoardGen
 }
 
-
-
-var svg = $("svg"); //Define element we will write to
-
-svg.html(gameBoardGen); //render board from gameBoardGen
+drawBoard();
